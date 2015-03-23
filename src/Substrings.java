3cdb4e2676c7a02a7cc1499d65d78bb9a402240b
aasatt <aasatt@gmail.com>
2015-03-23 14:03:23 -0700

    Problem 1
    completed

--- src/Substrings.java
+++ src/Substrings.java
@@ -1,4 +1,16 @@
+//Homework Problem #1
+import java.util.Scanner;
 
 public class Substrings {
-
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a word: ");
+		String word = input.next();
+		
+		for(int i = 1; i <= word.length(); i++) {
+			for (int j = 1; j <= word.length()-i+1; j++) {
+				System.out.println(word.substring(j-1, j+i-1));
+			}
+		}
+	}
 }
