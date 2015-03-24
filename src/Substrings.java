02a1e667a957a526deb995f2cc963a865cff44ab
Derrick Nguyen <derrickhnguyen@outlook.com>
2015-03-23 23:56:33 -0700

    Starting

--- src/Substrings.java
+++ src/Substrings.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class Substrings {
-
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.println("This program will read a word and print all substring, sorted by length");
+		
+		System.out.println("-----------------------------------------------------------------------");
+		System.out.print("Enter a word: ");
+		String word = input.next();
+		
+		for(int i = 0; i < word.length(); i++){
+			System.out.println(word.substring(i, i+1));
+			while(i < word.length()){
+				
+			}
+		}
+	}
 }
