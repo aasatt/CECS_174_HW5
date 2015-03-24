32133d25c76e1c41f4856a6eb3514f34845f70e6
aasatt <aasatt@gmail.com>
2015-03-24 00:20:10 -0700

    Problem 2 Done

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,4 +1,48 @@
+import java.util.Scanner;
+
+
 
 public class PrimeNumbers {
+	public static void main (String[] args) {
+		System.out.println("Enter an integer: ");
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		PrimePrinter pP = new PrimePrinter(n);
+	}
+}
+
+class PrimePrinter {
+	
+	public PrimePrinter(int n) {
+		PrintGenerator pGen = new PrintGenerator(n);
+		int primeNum = pGen.nextPrime();
+		while (primeNum < n) {
+			System.out.print(primeNum+" ");
+			primeNum = pGen.nextPrime();
+		}
+	}
+}
+
+class PrintGenerator {
+	private int n = 0;
+	private int count = 2;
+	public PrintGenerator(int input) {
+		n = input;
+	}
+	public int nextPrime() {
+		while (!this.isPrime(count) && count<n) {
+			count++;
+		}
+		count++;
+		return count-1;
+	}
+	public boolean isPrime(int test) {
+		for (int i=2; i<test; i++) {
+			if (test%i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
